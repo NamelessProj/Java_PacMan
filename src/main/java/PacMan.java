020158e04a13729b78620fcb8385519b8036b34c
Kevin <pintokevin2002@hotmail.com>
2025-05-19 22:06:21 +0200

    Remove unused import statement from PacMan.java

--- src/main/java/PacMan.java
+++ src/main/java/PacMan.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Random;
 
 public class PacMan extends JPanel implements ActionListener, KeyListener {
