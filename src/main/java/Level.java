1fd97434fe16dbdbfbbca80add538a537bbd7005
Pinto <pintokevin2002@hotmail.com>
2025-05-20 13:05:26 +0200

    Add LEVEL_3 layout to Level.java and update getLevel method

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -50,17 +50,42 @@ public class Level {
             "XXXXXXXXXOXXXXXXXXX"
     };
 
+    public static final String[] LEVEL_3 = {
+            "XXXXXXXXXOXXXXXXXXX",
+            "X  X           X  X",
+            "X XX XXX X XXX XX X",
+            "X                 X",
+            "X XXXX XX  X XXXX X",
+            "X      X   X      X",
+            "XXXX XXX  XXXX XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXrXX X XXXX",
+            "O       bpo       O",
+            "XXXX X XXXXX X XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXXXX X XXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X  X     P     X  X",
+            "XX X X XXXXX X X XX",
+            "X    X   X   X    X",
+            "X XXXXXX X XXXXXX X",
+            "X                 X",
+            "XXXXXXXXXOXXXXXXXXX"
+    };
+
 
 
     /**
      * Get the level layout based on the level number.
-     * @param level the level number (1 or 2)
+     * @param level the level number (1 to 3)
      * @return the level layout as a String array
      */
     public static String[] getLevel(int level) {
         return switch (level) {
             case 1 -> LEVEL_1;
             case 2 -> LEVEL_2;
+            case 3 -> LEVEL_3;
             default -> LEVEL_1; // Default to level 1 if the level is not recognized
         };
     }
